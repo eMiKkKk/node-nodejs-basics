982a602623f6967f799a17f8e9e0ed266918b73c
eMiKkKk <eMiKk.eMiKk@gmail.com>
2025-04-24 15:51:58 +0300

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    let index = '';
+    for(let i = 2; i < process.argv.length; i++) {
+        i % 2 == 0 ? index += `${process.argv[i].replace('--', '')} is `: index += `${process.argv[i].replace('--', '')}, `;
+    }
+    console.log(index);
 };
 
 parseArgs();
