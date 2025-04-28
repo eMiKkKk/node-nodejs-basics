444911fedb1989deec8f540ef7f9a6019e638a95
eMiKkKk <eMiKk.eMiKk@gmail.com>
2025-04-28 22:55:58 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import {pipeline} from 'stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+
+    const readStream = fs.createReadStream('./files/fileToCompress.txt');
+    const writeStream = fs.createWriteStream('./files/archive.gz');
+    const gzip = zlib.createGzip();
+
+    try {
+        await pipeline(
+            readStream,
+            gzip,
+            writeStream
+        );
+        console.log('success!');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import {pipeline} from 'stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+
+    const readStream = fs.createReadStream('./files/archive.gz');
+    const writeStream = fs.createWriteStream('./files/fileToCompress.txt');
+    const gunzip = zlib.createGunzip();
+
+    try {
+        await pipeline(
+            readStream,
+            gunzip,
+            writeStream
+        );
+        console.log('success!');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 };
 
 await decompress();
